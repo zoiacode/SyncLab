7e568f6f5f4e8ed334a0bfc08d1e4bbf06f7c0b3
Guilherme Zuim <126419420+zoiacode@users.noreply.github.com>
2025-11-27 20:37:23 -0300

    v 0.6

--- src/main/java/util/Cors.java
+++ src/main/java/util/Cors.java
@@ -6,22 +6,34 @@ import static spark.Spark.options;
 public class Cors {
     public static void enableCORS() {
         before((req, res) -> {
-            res.header("Access-Control-Allow-Origin", "https://sync-lab-front-olive.vercel.app");
+            res.header("Access-Control-Allow-Origin",
+                    "https://sync-lab-front-olive.vercel.app");
             res.header("Access-Control-Allow-Credentials", "true");
-            res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-            res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+
+            res.header("Access-Control-Allow-Headers",
+                    "Content-Type, Authorization, Cookie, Set-Cookie, X-Requested-With");
+
+            res.header("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS");
+
+            res.header("Access-Control-Expose-Headers", "Set-Cookie");
         });
 
         options("/*", (req, res) -> {
             res.status(200);
 
-            String headers = req.headers("Access-Control-Request-Headers");
-            if (headers != null)
-                res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            String reqHeaders = req.headers("Access-Control-Request-Headers");
+            if (reqHeaders != null)
+                res.header("Access-Control-Allow-Headers", reqHeaders);
 
-            String methods = req.headers("Access-Control-Request-Method");
-            if (methods != null)
-                res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            String reqMethod = req.headers("Access-Control-Request-Method");
+            if (reqMethod != null)
+                res.header("Access-Control-Allow-Methods", reqMethod);
+
+            res.header("Access-Control-Allow-Origin",
+                    "https://sync-lab-front-olive.vercel.app");
+
+            res.header("Access-Control-Allow-Credentials", "true");
 
             return "OK";
         });
