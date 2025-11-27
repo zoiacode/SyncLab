fc67d1d57bda203ade0fe8590e9dfab8434e19a5
Guilherme Zuim <126419420+zoiacode@users.noreply.github.com>
2025-11-27 20:41:09 -0300

    v 0.4

--- src/main/java/util/Cors.java
+++ src/main/java/util/Cors.java
@@ -6,34 +6,23 @@ import static spark.Spark.options;
 public class Cors {
     public static void enableCORS() {
         before((req, res) -> {
-            res.header("Access-Control-Allow-Origin",
-                    "https://sync-lab-front-olive.vercel.app");
-            res.header("Access-Control-Allow-Credentials", "true");
-
-            res.header("Access-Control-Allow-Headers",
-                    "Content-Type, Authorization, Cookie, Set-Cookie, X-Requested-With");
-
-            res.header("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, DELETE, OPTIONS");
-
+            res.header("Access-Control-Allow-Origin", "https://sync-lab-front-olive.vercel.app");
             res.header("Access-Control-Expose-Headers", "Set-Cookie");
+            res.header("Access-Control-Allow-Credentials", "true");
+            res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         });
 
         options("/*", (req, res) -> {
             res.status(200);
 
-            String reqHeaders = req.headers("Access-Control-Request-Headers");
-            if (reqHeaders != null)
-                res.header("Access-Control-Allow-Headers", reqHeaders);
+            String headers = req.headers("Access-Control-Request-Headers");
+            if (headers != null)
+                res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie, Set-Cookie");
 
-            String reqMethod = req.headers("Access-Control-Request-Method");
-            if (reqMethod != null)
-                res.header("Access-Control-Allow-Methods", reqMethod);
-
-            res.header("Access-Control-Allow-Origin",
-                    "https://sync-lab-front-olive.vercel.app");
-
-            res.header("Access-Control-Allow-Credentials", "true");
+            String methods = req.headers("Access-Control-Request-Method");
+            if (methods != null)
+                res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie, Set-Cookie");
 
             return "OK";
         });
