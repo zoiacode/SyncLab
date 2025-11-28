420b5b334dd383617cf80f49743e811e687803fb
Pedro Italo B.C. <pedroitalobc@gmail.com>
2025-11-28 01:18:24 -0300

    bb

--- src/main/java/util/Cors.java
+++ src/main/java/util/Cors.java
@@ -5,22 +5,20 @@ import static spark.Spark.options;
 
 public class Cors {
     public static void enableCORS() {
+        String origin = "https://sync-lab-front-fifz5d8uk-pedroitalobcs-projects.vercel.app";
+
         before((req, res) -> {
-            res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+            res.header("Access-Control-Allow-Origin", origin);
             res.header("Access-Control-Allow-Credentials", "true");
             res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
             res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         });
 
         options("/*", (req, res) -> {
-            res.status(200);
-
-            String headers = req.headers("Access-Control-Request-Headers");
-            if (headers != null) res.header("Access-Control-Allow-Headers", headers);
-
-            String methods = req.headers("Access-Control-Request-Method");
-            if (methods != null) res.header("Access-Control-Allow-Methods", methods);
-            
+            res.header("Access-Control-Allow-Origin", origin);
+            res.header("Access-Control-Allow-Credentials", "true");
+            res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
             return "OK";
         });
     }
